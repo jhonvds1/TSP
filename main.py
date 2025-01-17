09dd5d639cc6d5b430f864c6f38ee2ebfa6964fe
jonatha silva <jonatha.sviegas@hotmail.com>
2025-01-17 17:03:15 -0300

    complidado

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-with open('tsp1_253.txt','r',encoding='utf-8') as caminho:
-    linhas = caminho.readlines()
-caminhos = [list(map(int,linha.split())) for linha in linhas]
+def abrir_caminho():
+    with open('tsp1_253.txt','r',encoding='utf-8') as caminho:
+        linhas = caminho.readlines()
+    caminhos = [list(map(int,linha.split())) for linha in linhas]
+    return caminhos
 
+def inicializacao_bb():
+    caminhos = abrir_caminho()
+    caminhos_aux=caminhos
+    
+
+inicializacao_bb()
 
 
