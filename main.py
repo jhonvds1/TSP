d893245306538b3ba6ce8f82bd46ef05762c6a56
jonatha silva <jonatha.sviegas@hotmail.com>
2025-01-20 15:21:46 -0300

    teste

--- main.py
+++ main.py
@@ -13,37 +13,3 @@ def inicializacao_bb():
     caminhos_aux=caminhos      
     return caminhos
 
-def calcular_distancia(x1,x2,y1,y2):
-    return math.sqrt((x1-x2)**2+(y1-y2)**2)  # Calcula a distância entre dois pontos da matriz
-
-
-os.system('cls')
-caminho=inicializacao_bb()
-
-while True:
-    try:
-        x_inicial=input("Digite o 'x' inicial da matriz ")
-        x_inicial=int(x_inicial)
-        y_inicial=input("Digite o 'y' inicial da matriz ")
-        y_inicial=int(y_inicial)
-        if x_inicial>(len(caminho[0])-1) or x_inicial<0 or y_inicial>(len(caminho)-1) or y_inicial<0:
-            print('Posição fora do range')
-        else:
-            caminho[x_inicial][y_inicial]=-2
-            break
-    except ValueError:                                                        #Lê e verifica se a entrada é aceita
-        print('Digite um número inteiro')
-        time.sleep(2)
-        os.system('cls')
-
-
-    
-for linha in caminho:
-    print(linha)
-    print()
-
-
-
-
-
-
